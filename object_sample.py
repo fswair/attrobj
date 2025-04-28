6f0a0c0ca5f5a9be68029695afd73fcc00943012
Mert <62549656+fswair@users.noreply.github.com>
2025-04-28 03:09:54 +0300

    Create object_sample.py

---
+++ object_sample.py
@@ -0,0 +1,15 @@
+from object import Object
+
+data = Object({"quake": {"mw": "3.7"})
+data2 = Object({"quake": {"ml": "3.9"})
+
+print("first quake:", data.any(["mw", "ml", "md"]), key=True) # first quake: {mw: 3.7}
+print("second quake:", data2.any(["mw", "ml", "md"]), key=True) # second quake: {ml: 3.9}
+
+data.haskey("quake") # has data 'quake' key?
+data.only() # bring only values that are not none
+data.only(lambda k, v: k[:2] == "is") # bring only values that are start with 'is' substring
+
+info = Object({"info": {"quake": {"magnitude": 3.9}}})
+
+print(info.info.quake.magnitude) # 3.9
