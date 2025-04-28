58ba76e982d36d135a702cfecd7053b185361c76
Mert <62549656+fswair@users.noreply.github.com>
2025-04-28 03:13:10 +0300

    Update object_sample.py

--- object_sample.py
+++ object_sample.py
@@ -1,10 +1,10 @@
-from object import Object
+from moderndict import Object
 
-data = Object({"quake": {"mw": "3.7"})
-data2 = Object({"quake": {"ml": "3.9"})
+data = Object({"quake": {"mw": "3.7"}})
+data2 = Object({"quake": {"ml": "3.9"}})
 
-print("first quake:", data.any(["mw", "ml", "md"]), key=True) # first quake: {mw: 3.7}
-print("second quake:", data2.any(["mw", "ml", "md"]), key=True) # second quake: {ml: 3.9}
+print("first quake:", data.quake.any(["mw", "ml", "md"], key=True)) # first quake: {mw: 3.7}
+print("second quake:", data2.quake.any(["mw", "ml", "md"], key=True)) # second quake: {ml: 3.9}
 
 data.haskey("quake") # has data 'quake' key?
 data.only() # bring only values that are not none
